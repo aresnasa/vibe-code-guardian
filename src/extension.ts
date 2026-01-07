f33628732c7a302d3d0c7ad2f34073e3f9fb38e8
aresnasa <aresnasa@126.com>
2026-01-07 21:44:40 +0800

    [Vibe Guardian] 📸 Auto-save @ 01/07 21:44 (src/extension.ts)

--- src/extension.ts
+++ src/extension.ts
@@ -11,7 +11,7 @@ import { RollbackManager, DiffContentProvider } from './rollbackManager';
 import { TimelineTreeProvider, TimelineItem } from './timelineTreeProvider';
 import { StateMonitor } from './stateMonitor';
 import { CheckpointType, CheckpointSource, ChangedFile, FileChangeType, CommitLanguage, NotificationLevel } from './types';
-import { getLanguageDisplayName, getNextLanguage } from './languageConfig';
+import { getLanguageDisplayName, getNextLanguage, getNotificationLevelDisplayName, getNextNotificationLevel } from './languageConfig';
 
 /**
  * Determines if a notification should be shown based on notification level and checkpoint type
